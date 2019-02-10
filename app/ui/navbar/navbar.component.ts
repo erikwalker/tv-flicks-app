7d7a4cc010e845bd77c84a05213cf59eef353d2e
erik walker <erik.walker2016@gmail.com>
2019-02-10 18:57:40 -0500

    -add implementation of tv show search and listing of episodes -add connections to external website and api calls to tvmaze.com

---
+++ app/ui/navbar/navbar.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { NavItem } from './navItems';
+
+@Component({
+  moduleId: module.id,
+  selector: 'tv-navbar',
+  templateUrl: 'navbar.template.html'
+})
+
+export class NavbarComponent {
+  appName: string = "TV Flix App";
+
+  navItems: NavItem[] = [
+     { routerLink: '/', label: 'Home', active: false },
+     { routerLink: '/search', label: 'Find Shows', active: false }
+   ];
+
+}
